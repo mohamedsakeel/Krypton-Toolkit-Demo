101febb510697113e2631bae59e3e7e185abf93d
MetaAttack <49219375+mohamedsakeel@users.noreply.github.com>
2021-09-15 12:33:44 +0530

    added solution with the project

---
+++ Krypron Toolkit Demo/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Krypron_Toolkit_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.kryptonCheckBox1 = new ComponentFactory.Krypton.Toolkit.KryptonCheckBox();
+            this.kryptonComboBox1 = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            this.kryptonDropButton1 = new ComponentFactory.Krypton.Toolkit.KryptonDropButton();
+            this.kryptonSeparator1 = new ComponentFactory.Krypton.Toolkit.KryptonSeparator();
+            this.kryptonWorkspace1 = new ComponentFactory.Krypton.Workspace.KryptonWorkspace();
+            this.kryptonTrackBar1 = new ComponentFactory.Krypton.Toolkit.KryptonTrackBar();
+            this.kryptonBreadCrumb1 = new ComponentFactory.Krypton.Toolkit.KryptonBreadCrumb();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonWorkspace1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonBreadCrumb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.FormStyles.FormCommon.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormCommon.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormCommon.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.FormStyles.FormCommon.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.kryptonPalette1.FormStyles.FormCommon.StateCommon.Border.Rounding = 12;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            // 
+            // kryptonCheckBox1
+            // 
+            this.kryptonCheckBox1.Location = new System.Drawing.Point(88, 79);
+            this.kryptonCheckBox1.Name = "kryptonCheckBox1";
+            this.kryptonCheckBox1.Size = new System.Drawing.Size(125, 20);
+            this.kryptonCheckBox1.TabIndex = 0;
+            this.kryptonCheckBox1.Values.Text = "kryptonCheckBox1";
+            // 
+            // kryptonComboBox1
+            // 
+            this.kryptonComboBox1.DropDownWidth = 121;
+            this.kryptonComboBox1.Location = new System.Drawing.Point(92, 115);
+            this.kryptonComboBox1.Name = "kryptonComboBox1";
+            this.kryptonComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.kryptonComboBox1.TabIndex = 1;
+            this.kryptonComboBox1.Text = "kryptonComboBox1";
+            // 
+            // kryptonDropButton1
+            // 
+            this.kryptonDropButton1.Location = new System.Drawing.Point(108, 167);
+            this.kryptonDropButton1.Name = "kryptonDropButton1";
+            this.kryptonDropButton1.Size = new System.Drawing.Size(90, 25);
+            this.kryptonDropButton1.TabIndex = 2;
+            this.kryptonDropButton1.Values.Text = "kryptonDropButton1";
+            // 
+            // kryptonSeparator1
+            // 
+            this.kryptonSeparator1.Location = new System.Drawing.Point(334, 37);
+            this.kryptonSeparator1.Name = "kryptonSeparator1";
+            this.kryptonSeparator1.Size = new System.Drawing.Size(18, 401);
+            this.kryptonSeparator1.TabIndex = 3;
+            // 
+            // kryptonWorkspace1
+            // 
+            this.kryptonWorkspace1.ContainerBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.HeaderPrimary;
+            this.kryptonWorkspace1.Location = new System.Drawing.Point(1, 12);
+            this.kryptonWorkspace1.Name = "kryptonWorkspace1";
+            this.kryptonWorkspace1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue;
+            // 
+            // 
+            // 
+            this.kryptonWorkspace1.Root.UniqueName = "500F5C87AB644D85CBA388998626E013";
+            this.kryptonWorkspace1.Root.WorkspaceControl = this.kryptonWorkspace1;
+            this.kryptonWorkspace1.SeparatorStyle = ComponentFactory.Krypton.Toolkit.SeparatorStyle.HighInternalProfile;
+            this.kryptonWorkspace1.Size = new System.Drawing.Size(663, 26);
+            this.kryptonWorkspace1.TabIndex = 4;
+            this.kryptonWorkspace1.TabStop = true;
+            // 
+            // kryptonTrackBar1
+            // 
+            this.kryptonTrackBar1.DrawBackground = true;
+            this.kryptonTrackBar1.Location = new System.Drawing.Point(68, 258);
+            this.kryptonTrackBar1.Name = "kryptonTrackBar1";
+            this.kryptonTrackBar1.Size = new System.Drawing.Size(167, 27);
+            this.kryptonTrackBar1.TabIndex = 5;
+            // 
+            // kryptonBreadCrumb1
+            // 
+            this.kryptonBreadCrumb1.AutoSize = false;
+            this.kryptonBreadCrumb1.Location = new System.Drawing.Point(59, 208);
+            this.kryptonBreadCrumb1.Name = "kryptonBreadCrumb1";
+            // 
+            // 
+            // 
+            this.kryptonBreadCrumb1.RootItem.ShortText = "Root";
+            this.kryptonBreadCrumb1.Size = new System.Drawing.Size(200, 28);
+            this.kryptonBreadCrumb1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(676, 450);
+            this.Controls.Add(this.kryptonBreadCrumb1);
+            this.Controls.Add(this.kryptonTrackBar1);
+            this.Controls.Add(this.kryptonWorkspace1);
+            this.Controls.Add(this.kryptonSeparator1);
+            this.Controls.Add(this.kryptonDropButton1);
+            this.Controls.Add(this.kryptonComboBox1);
+            this.Controls.Add(this.kryptonCheckBox1);
+            this.Name = "Form1";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UI Toolkit";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonWorkspace1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonBreadCrumb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private ComponentFactory.Krypton.Toolkit.KryptonCheckBox kryptonCheckBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonDropButton kryptonDropButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonSeparator kryptonSeparator1;
+        private ComponentFactory.Krypton.Workspace.KryptonWorkspace kryptonWorkspace1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTrackBar kryptonTrackBar1;
+        private ComponentFactory.Krypton.Toolkit.KryptonBreadCrumb kryptonBreadCrumb1;
+    }
+}
+
